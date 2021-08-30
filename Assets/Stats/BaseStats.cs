1caa7adf11cd844d80b8d9dfd3bff21945e670a3
KimWonseop <won0539@gmail.com>
2021-08-30 21:55:40 +0900

    Add character class progression

---
+++ Assets/Stats/BaseStats.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats {
+    public class BaseStats : MonoBehaviour {
+        [Range(1, 99)]
+        [SerializeField]
+        private int level = 1;
+        [SerializeField]
+        private CharacterClass characterClass;
+        [SerializeField]
+        private Progression progression;
+    }
+}
+
---
+++ Assets/Stats/Progression.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RPG.Stats {
+    [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
+    public class Progression : ScriptableObject {
+        [SerializeField]
+        private ProgressionCharacterClass[] characterClass = null;
+
+        [System.Serializable]
+        class ProgressionCharacterClass {
+            [SerializeField]
+            private CharacterClass characterClass;
+            [SerializeField]
+            private float[] health;
+        }
+    }
+}
+
